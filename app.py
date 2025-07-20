86c63406494cd6e15dc4749b740a97356381c002
EddyGonzalo <105296156+EddyGonzalo@users.noreply.github.com>
2025-07-20 15:38:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -153,4 +153,4 @@ def estadisticas():
                            promedio_edad=promedio_edad, historial=historial, active_page='estadisticas')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
